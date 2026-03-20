18c13a028d42d78d850ad6102a2a559c63319c80
Alie2025 <3184264a@student.gla.ac.uk>
2026-03-20 16:41:30 +0000

    Update apache_booking_1.py

--- apache_booking_1.py
+++ apache_booking_1.py
@@ -1,7 +1,7 @@
 """
 ================================================================
   Apache Airlines — Burak757 Seat Booking System
-  FC723 Programming Theory  |  Project Part A
+  FC723 Programming Theory  |  Project Part B
 ================================================================
  
 PURPOSE:
